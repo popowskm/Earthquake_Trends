7aae436e434d840d73a6a42d9e38477df04a257f
Mathew <mathew.popowski@gmail.com>
2020-01-03 14:31:21 -0800

    Resolves scatter plot color warnings

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from sklearn import metrics
 
 filename = 'earthquake_formatted_data_partial_utm.txt'
 data = np.loadtxt('data/' + filename,skiprows=1)
-newdata = StandardScaler().fit_transform(data)
-clustering = DBSCAN(eps=0.3, min_samples=5, metric='euclidean').fit(newdata)
+# newdata = StandardScaler().fit_transform(data)
+data[:,0] = data[:,0]/5
+clustering = DBSCAN(eps=10000, min_samples=5, metric='euclidean').fit(data)
 core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
 core_samples_mask[clustering.core_sample_indices_]= True
 labels = clustering.labels_
 
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
-print(n_noise_)
+print("Number of noise points: ",n_noise_)
+print("Number of clusters: ", n_clusters_)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -24,13 +26,14 @@ colors = [plt.cm.get_cmap(name='plasma')(each)
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 for k, col in zip(unique_labels, colors):
+    color = [col]
     if k == -1:
-        col = [0,0,0,1]
+        color = [[0,0,0,1]]
     class_member_mask = (labels == k)
     xy = data[class_member_mask & core_samples_mask]
-    ax.scatter(xy[:,1], xy[:,2], xy[:,0], marker='o', s=17, c=tuple(col), edgecolor='k')
+    ax.scatter(xy[:,1], xy[:,2], xy[:,0], marker='o', s=17, c=color, edgecolor='k')
     xy = data[class_member_mask & ~core_samples_mask]
-    ax.scatter(xy[:, 1], xy[:, 2], xy[:,0], marker='o', c=tuple(col),
+    ax.scatter(xy[:, 1], xy[:, 2], xy[:,0], marker='o', c=color,
              edgecolor='k', s=6)
 # ax.scatter(data[:,1], data[:,2], data[:,0], s=1, c=[i for i,j in enumerate(labels)], cmap=plt.cm.get_cmap(name='Spectral'))
 plt.show()
