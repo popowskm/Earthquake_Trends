fdb2d026cb925acb70f5d5780e3235be3615b816
Mathew <mathew.popowski@gmail.com>
2020-01-03 14:43:15 -0800

    Cleans and comments code

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 
-filename = 'earthquake_formatted_data_partial_utm.txt'
+# Modifiable constants
+filename = 'earthquake_formatted_data_partial_utm.txt' # Data source file located in data folder
+dist = 10000 # Tolerance in ~distance of points during clustering
+samps = 5 # Number of samples required to constitute a cluster
+
 data = np.loadtxt('data/' + filename,skiprows=1)
-# newdata = StandardScaler().fit_transform(data)
-data[:,0] = data[:,0]/5
-clustering = DBSCAN(eps=10000, min_samples=5, metric='euclidean').fit(data)
+data[:,0] = data[:,0]/5 # Transform seconds to be in line with distance scale of x and y
+
+clustering = DBSCAN(eps=dist, min_samples=samps, metric='euclidean').fit(data)
+
+# Mask out all but core sample data
 core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
 core_samples_mask[clustering.core_sample_indices_]= True
-labels = clustering.labels_
 
+# Get stats about clustering data
+labels = clustering.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
 print("Number of noise points: ",n_noise_)
@@ -20,9 +27,11 @@ print("Number of clusters: ", n_clusters_)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+# Assign color to each cluster
 unique_labels = set(labels)
 colors = [plt.cm.get_cmap(name='plasma')(each)
         for each in np.linspace(0,1,len(unique_labels))]
+# Plot each cluster iteratively onto figure
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 for k, col in zip(unique_labels, colors):
@@ -30,10 +39,9 @@ for k, col in zip(unique_labels, colors):
     if k == -1:
         color = [[0,0,0,1]]
     class_member_mask = (labels == k)
+    # Core cluster points made larger
     xy = data[class_member_mask & core_samples_mask]
     ax.scatter(xy[:,1], xy[:,2], xy[:,0], marker='o', s=17, c=color, edgecolor='k')
     xy = data[class_member_mask & ~core_samples_mask]
-    ax.scatter(xy[:, 1], xy[:, 2], xy[:,0], marker='o', c=color,
-             edgecolor='k', s=6)
-# ax.scatter(data[:,1], data[:,2], data[:,0], s=1, c=[i for i,j in enumerate(labels)], cmap=plt.cm.get_cmap(name='Spectral'))
+    ax.scatter(xy[:, 1], xy[:,2], xy[:,0], marker='o', s=6, c=color, edgecolor='k')
 plt.show()
