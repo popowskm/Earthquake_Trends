32916b2909171e472e7463580db783e5964f7517
Mathew <mathew.popowski@gmail.com>
2020-02-23 17:41:23 -0800

    Gives option to avoid graphical display of data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn import metrics
 # Modifiable constants
 filename = 'earthquake_formatted_data_partial_utm.txt' # Data source file located in data folder
 outfile = 'earthquake_output_data_partial.txt' # Output file name
+display = False # Whether or not to provide graphical representation of data
 dist_min = 20000  # Initial # Tolerance in ~radius of points during clustering
 dist_max = 20001  # Final #
 dist_step = 1  # Step #
@@ -32,9 +33,10 @@ for dist in range(dist_min, dist_max, dist_step):
             clustering = DBSCAN(eps=dist, min_samples=samps, metric='euclidean').fit(data)
 
             # Mask out all but core sample data 
-            core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
-            core_samples_mask[clustering.core_sample_indices_]= True
-            core_samples_mask_master.append(core_samples_mask)
+            if display:
+                core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
+                core_samples_mask[clustering.core_sample_indices_]= True
+                core_samples_mask_master.append(core_samples_mask)
 
             # Get stats about clustering data
             labels.append(clustering.labels_)
@@ -42,34 +44,35 @@ for dist in range(dist_min, dist_max, dist_step):
 output = [(np.append(data_raw[k], [row[k] for row in labels])) for k, m in enumerate(data_raw)]
 np.savetxt("output/" + outfile, output, fmt="%.7f", header="time x y cluster_labels")
 # Currently uses inital conditions
-n_clusters_ = len(set(labels[0])) - (1 if -1 in labels[0] else 0)
-n_noise_ = list(labels[0]).count(-1)
-print("Number of noise points: ",n_noise_)
-print("Number of clusters: ", n_clusters_)
+if display:
+    n_clusters_ = len(set(labels[0])) - (1 if -1 in labels[0] else 0)
+    n_noise_ = list(labels[0]).count(-1)
+    print("Number of noise points: ",n_noise_)
+    print("Number of clusters: ", n_clusters_)
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-# Assign color to each cluster
-unique_labels = set(labels[0])
-colors = [plt.cm.get_cmap(name='prism')(each)
-        for each in np.linspace(0,1,len(unique_labels))]
-# Plot each cluster iteratively onto figure
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-for k, col in zip(unique_labels, colors):
-    color = [col]
-    if k == -1:
-        color = [[0,0,0,1]]
-    class_member_mask = (labels[0] == k)
-    # Core cluster points made larger
-    xy = data[class_member_mask & core_samples_mask]
-    ax.scatter(xy[:,1], xy[:,2], xy[:,0]*scale_time, marker='o', s=17, c=color, edgecolor=color)
-    xy = data[class_member_mask & ~core_samples_mask]
-#    ax.scatter(xy[:, 1], xy[:,2], xy[:,0]*scale_time, marker='o', s=6, c=color, edgecolor=color)
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    # Assign color to each cluster
+    unique_labels = set(labels[0])
+    colors = [plt.cm.get_cmap(name='prism')(each)
+            for each in np.linspace(0,1,len(unique_labels))]
+    # Plot each cluster iteratively onto figure
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    for k, col in zip(unique_labels, colors):
+        color = [col]
+        if k == -1:
+            color = [[0,0,0,1]]
+        class_member_mask = (labels[0] == k)
+        # Core cluster points made larger
+        xy = data[class_member_mask & core_samples_mask]
+        ax.scatter(xy[:,1], xy[:,2], xy[:,0]*scale_time, marker='o', s=17, c=color, edgecolor=color)
+        xy = data[class_member_mask & ~core_samples_mask]
+    #    ax.scatter(xy[:, 1], xy[:,2], xy[:,0]*scale_time, marker='o', s=6, c=color, edgecolor=color)
 
-ax.set_xlabel('UTM X')
-ax.set_ylabel('UTM Y')
-ax.set_zlabel('time')
-ax.view_init(50,0)  # South to North section
-# ax.view_init(90,-90)  # Top view
-plt.show()
+    ax.set_xlabel('UTM X')
+    ax.set_ylabel('UTM Y')
+    ax.set_zlabel('time')
+    ax.view_init(50,0)  # South to North section
+    # ax.view_init(90,-90)  # Top view
+    plt.show()
